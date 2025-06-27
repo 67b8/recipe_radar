81ce3af31a94a7efe6abbc9eb174f84cd6980f95
67b8 <149882064+67b8@users.noreply.github.com>
2025-06-27 20:59:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -30,6 +30,6 @@ mongoose.connect(uri).then(()=>{
     console.log("connection successful")
 }).catch(()=>{
     console.log(uri)
-    console.log("connection unsuccessful")
+    // console.log("connection unsuccessful")
 })
 
