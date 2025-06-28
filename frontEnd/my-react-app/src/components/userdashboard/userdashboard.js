6394ca481bf032d15e22b99a955bf4346bc4fcca
67b8 <149882064+67b8@users.noreply.github.com>
2025-06-28 13:33:39 +0530

    fixed the url and deleted commented code

--- frontEnd/my-react-app/src/components/userdashboard/userdashboard.js
+++ frontEnd/my-react-app/src/components/userdashboard/userdashboard.js
@@ -1,42 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import AppDownload from '../AppDownload/AppDownload';
-// import Carousel from '../Carousel/carousel';
-// import Footer from '../Footer/Footer';
-// import RecipeCard from '../RecipeCard/RecipeCard';
-// import Usernavbar from '../usernavbar/usernavbar';
-
-// export default function Userdashboard() {
-//   const [recipes, setRecipes] = useState([]);
-
-//   useEffect(() => {
-//     fetch('https://recipeserver-67b8s-projects.vercel.app/api/recipes')
-//       .then(response => response.json())
-//       .then(data => setRecipes(data))
-//       .catch(error => console.error('Error fetching recipes:', error));
-//   }, []);
-
-//   return (
-//     <>
-//       <Usernavbar />
-//       <Carousel />
-//       <div className="container mt-4 userdashboard" id="userdashboard">
-//         <h2 className="text-center mb-4">ALL RECIPES</h2>
-//         <div className="d-flex flex-wrap justify-content-around">
-//           {recipes.map((recipe, index) => (
-//             <Link key={index} className="nav-link" to={`/recipe/${recipe._id}`}>
-//               <div className="grid-item mb-3" style={{ width: "236px", height: "375px" ,borderRadius:'0'}}>
-//               <RecipeCard recipename={recipe.name} description={`Let's make amazing ${recipe.name}`} averageRating={recipe.averageRating} image={recipe.image}/>
-//               </div>
-//             </Link>
-//           ))}
-//         </div>
-//       </div>
-//       <AppDownload />
-//       <Footer />
-//     </>
-//   );
-// }
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import AppDownload from '../AppDownload/AppDownload';
@@ -59,7 +20,7 @@ export default function Userdashboard() {
   const fetchRecommendedRecipes = async (cuisines, dietaryRestrictions) => {
     try {
       const recipePromises = cuisines.map(cuisine =>
-        axios.get(`http://localhost:3001/api/recipes/search/cuisine/${cuisine}`)
+        axios.get(`https://recipeserver-67b8s-projects.vercel.app/api/recipes/search/cuisine/${cuisine}`)
       );
       const recipeResponses = await Promise.all(recipePromises);
       let fetchedRecipes = recipeResponses.flatMap(response => response.data);
